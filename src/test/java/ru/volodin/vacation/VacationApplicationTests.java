e74e57265c75a7224b31801a84615ba07626dfb0
HzAjax <danil3000200@mail.ru>
2025-04-01 19:21:46 +0400

    Init coomit

---
+++ src/test/java/ru/volodin/vacation/VacationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.volodin.vacation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
